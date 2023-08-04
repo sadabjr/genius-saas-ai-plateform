9b18b4dc02e4cff9bac292a77ac6df945e7c5af5
Sadab Ali <84293105+sadabjr@users.noreply.github.com>
2023-08-04 09:20:33 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+     <p className='text-6xl'>
+    hello
+   </p>
+   <Button>Click me</Button>
+    </>
+  
+  )
+}
